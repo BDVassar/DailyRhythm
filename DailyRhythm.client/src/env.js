1ecd7fcfe14f36622c3f49f83d75e94a07b90911
Bryan Vassar <vassar.bryan@gmail.com>
2022-12-21 10:34:50 -0700

    init/env updated and checked

---
+++ DailyRhythm.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'bryan-vassar.us.auth0.com'
+export const clientId = 'wbVaUZk4wdIqMXWLCnNUKKznQbywQkzf'
+export const audience = 'https://bryanclassroom.com'
