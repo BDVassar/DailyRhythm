5a003d9fc9af311ba55da64190cfe054ad906e97
Jordan Diaz <jordanldiaz29@gmail.com>
2022-12-21 17:11:54 -0700

    updated quote api

--- DailyRhythm.client/src/services/AxiosService.js
+++ DailyRhythm.client/src/services/AxiosService.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import Axios from 'axios'
 import { baseURL } from '../env'
 export const api = Axios.create({
@@ -11,5 +12,13 @@ export const UnsplashApi = Axios.create({
   params: {
     client_id: 'Nn9OBE0iNJvFJop3A8lI8DACrD-Pmqk3OUqabk1tEx8',
     per_page: 1
+  }
+})
+
+export const zenQuoteAPI = Axios.create({
+  baseURL: 'https://zenquotes.io/api ',
+  timeout: 8000,
+  params: {
+
   }
 })
