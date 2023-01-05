d8b9544873147e521b1476345c8c9d44d9529a0c
Jordan Diaz <jordanldiaz29@gmail.com>
2023-01-05 13:00:53 -0700

    secret keys hidden

--- DailyRhythm.client/src/services/AxiosService.js
+++ DailyRhythm.client/src/services/AxiosService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import Axios from 'axios'
-import { baseURL } from '../env'
+import { baseURL, appid, client_id } from '../env'
 export const api = Axios.create({
   baseURL,
   timeout: 8000
@@ -10,7 +10,7 @@ export const UnsplashApi = Axios.create({
   baseURL: 'https://api.unsplash.com/search/photos/',
   timeout: 8000,
   params: {
-    client_id: 'Nn9OBE0iNJvFJop3A8lI8DACrD-Pmqk3OUqabk1tEx8'
+    client_id: client_id
   }
 })
 
@@ -22,7 +22,7 @@ export const WeatherApi = Axios.create({
   baseURL: 'https://api.openweathermap.org/data/2.5/weather?q=boise,id,us',
   timeout: 8000,
   params: {
-    appid: '38096cd1b875d3f48b111b3967e4f793'
+    appid: appid
   }
 })
 
