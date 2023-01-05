6f7ecaf28676b7ed30b030acc4066cefe6be4c84
Jordan Diaz <jordanldiaz29@gmail.com>
2023-01-05 14:03:09 -0700

    Home button on daily rhythm

--- DailyRhythm.client/src/pages/DailyRhythmPage.vue
+++ DailyRhythm.client/src/pages/DailyRhythmPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="component">
-
+  <div class="container-fluid">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <h1 class="col-3 d-flex justify-content-start text-white"><i class="mdi mdi-home-circle-outline"></i>
+      </h1>
+    </router-link>
 
   </div>
 </template>
@@ -10,8 +13,8 @@
 import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 export default {
-  setup(){
-  return {  }
+  setup() {
+    return {}
   }
 };
 </script>
