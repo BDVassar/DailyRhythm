2c1ee620c8dd16798f7977fb30a25de89cd9ef58
Kayla Cammack <kaylacammack@gmail.com>
2023-01-05 13:31:15 -0700

    Added comments to env for api keys

--- DailyRhythm.client/src/env.js
+++ DailyRhythm.client/src/env.js
@@ -4,5 +4,7 @@ export const useSockets = false
 export const domain = 'bryan-vassar.us.auth0.com'
 export const clientId = 'wbVaUZk4wdIqMXWLCnNUKKznQbywQkzf'
 export const audience = 'https://bryanclassroom.com'
+//Weather API key
 export const appid = 'cff3a95bc2ea79071a521f836aa63be6'
+//Unsplash background image API key
 export const client_id = 'rcMDFdxvSUY7MUWGst_db34L6rRNyVe0IJ8UAadgWWs'
