187c75a3c0bdfdf3e7fd0133f564bf7d40333dc9
Jordan Diaz <jordanldiaz29@gmail.com>
2023-01-05 13:05:52 -0700

    Updated weather api key

--- DailyRhythm.client/src/env.js
+++ DailyRhythm.client/src/env.js
@@ -4,5 +4,5 @@ export const useSockets = false
 export const domain = 'bryan-vassar.us.auth0.com'
 export const clientId = 'wbVaUZk4wdIqMXWLCnNUKKznQbywQkzf'
 export const audience = 'https://bryanclassroom.com'
-export const appid = '38096cd1b875d3f48b111b3967e4f793'
+export const appid = 'cff3a95bc2ea79071a521f836aa63be6'
 export const client_id = 'Nn9OBE0iNJvFJop3A8lI8DACrD-Pmqk3OUqabk1tEx8'
