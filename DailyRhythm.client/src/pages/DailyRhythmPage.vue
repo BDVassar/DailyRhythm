b0a68e3061d233cefbdbffa578588f21a7cf02b1
Kayla Cammack <kaylacammack@gmail.com>
2023-01-09 11:48:28 -0700

    Goal modal set up on DailyRhythmPage

--- DailyRhythm.client/src/pages/DailyRhythmPage.vue
+++ DailyRhythm.client/src/pages/DailyRhythmPage.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="container-fluid">
-    <div class="row justify-content-between">
-      <router-link class="col-2 navbar-brand d-flex justify-content-start" :to="{ name: 'Home' }">
-        <h1 class="text-white" title="Home"><i class="mdi mdi-home-circle-outline"></i>
-        </h1>
-      </router-link>
-      <div class="col-2 d-flex justify-content-end">
-        <h1 class=" text-white" title="Add New Goal"><i class="mdi mdi-plus-circle-outline"></i>
-        </h1>
-      </div>
-    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-between">
+            <router-link class="col-2 navbar-brand d-flex justify-content-start" :to="{ name: 'Home' }">
+                <h1 class="text-white" title="Home"><i class="mdi mdi-home-circle-outline"></i>
+                </h1>
+            </router-link>
+            <div class="col-2 d-flex justify-content-end">
 
-    <div class="row my-51">
-      <WeeklyCalendarView />
+                <h1 class=" text-white" title="Add New Goal"><button data-bs-toggle="modal"
+                        data-bs-target="#goalModal"><i class="mdi mdi-plus-circle-outline"></i></button>
+                </h1>
+            </div>
+        </div>
+
+        <div class="row my-51">
+            <WeeklyCalendarView />
+        </div>
+        <div class="row my-5">
+            <DailyRhythmViewOptions />
+        </div>
     </div>
-    <div class="row my-5">
-      <DailyRhythmViewOptions />
-    </div>
-  </div>
+    <ModalComponent id="goalModal">
+        <GoalForm />
+    </ModalComponent>
+
 </template>
 
 
@@ -26,11 +32,13 @@ import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 import WeeklyCalendarView from "../components/WeeklyCalendarView.vue";
 import DailyRhythmViewOptions from "../components/DailyRhythmViewOptions.vue";
+import ModalComponent from "../components/ModalComponent.vue";
+import GoalForm from "../components/GoalForm.vue";
 export default {
-  setup() {
-    return {};
-  },
-  components: { WeeklyCalendarView, DailyRhythmViewOptions }
+    setup() {
+        return {};
+    },
+    components: { WeeklyCalendarView, DailyRhythmViewOptions, ModalComponent, GoalForm }
 };
 </script>
 
