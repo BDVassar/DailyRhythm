57e079b7f8b354a80b2aa6e12294a4126e7915b2
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-10 10:50:43 -0700

    GoalPage created and added to router, router-link created

---
+++ DailyRhythm.client/src/pages/GoalPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+
+
+  </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+export default {
+  setup(){
+  return {  }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- DailyRhythm.client/src/router.js
+++ DailyRhythm.client/src/router.js
@@ -24,7 +24,7 @@ const routes = [
   },
 
   {
-    path: '/DailyRhythmPage',
+    path: '/DailyRhythm',
     name: 'DailyRhythmPage',
     component: loadPage('DailyRhythmPage'),
     beforeEnter: authGuard
@@ -34,7 +34,13 @@ const routes = [
     name: 'Settings',
     component: loadPage('SettingsPage'),
     beforeEnter: authGuard
-  }
+  },
+  {
+    path: '/Goal',
+    name: 'GoalPage',
+    component: loadPage('GoalPage'),
+    beforeEnter: authGuard
+  },
 ]
 
 export const router = createRouter({
