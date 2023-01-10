271833f519637e46b555e2855532640b7cd7a697
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-10 16:48:31 -0700

    router-link for goals detail page added

--- DailyRhythm.client/src/components/GoalCard.vue
+++ DailyRhythm.client/src/components/GoalCard.vue
@@ -1,14 +1,16 @@
 <template>
-  <section class="row card opacity-75 text-center p-3" :style="{ backgroundColor: `${goal.color}` }">
-    <h3><span class="mdi" :class="goal.icon"></span>{{ goal.name }}</h3>
-    <p>{{ goal.description }}</p>
-    <div class="col-12 text-center p-3">
-      <h5>Rhythms</h5>
-    </div>
-    <div class="col-12 text-center p-3">
-      <h5>Beats</h5>
-    </div>
-  </section>
+  <router-link :to="{ name: 'GoalDetails', params: { goalId: goal.id } }">
+    <section class="row card opacity-75 text-center p-3" :style="{ backgroundColor: `${goal.color}` }">
+      <h3><span class="mdi" :class="goal.icon"></span>{{ goal.name }}</h3>
+      <p>{{ goal.description }}</p>
+      <div class="col-12 text-center p-3">
+        <h5>Rhythms</h5>
+      </div>
+      <div class="col-12 text-center p-3">
+        <h5>Beats</h5>
+      </div>
+    </section>
+  </router-link>
 </template>
 
 
