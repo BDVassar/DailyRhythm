729c45b124a94cf9caf8ac95f631788365bd8dd6
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-10 10:58:31 -0700

    home button added to goal page, about page updated

--- DailyRhythm.client/src/pages/GoalPage.vue
+++ DailyRhythm.client/src/pages/GoalPage.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="component">
+  <div class="container-fluid">
+    <div class="row">
+      <router-link class="navbar-brand" :to="{ name: 'Home' }">
+        <h1 class="col-3 d-flex justify-content-start text-white" title="Home"><i
+            class="mdi mdi-home-circle-outline"></i>
+        </h1>
+      </router-link>
+      <router-link class="col-1" :to="{ name: 'DailyRhythmPage' }">
+        <button class="btn" data-bs-dismiss="offcanvas"><i class="mdi mdi-bullseye-arrow text-white"
+            title="Daily Rhythm Page" style="font-size: 40px"></i>
+        </button>
+      </router-link>
+    </div>
 
 
   </div>
@@ -8,10 +20,10 @@
 
 <script>
 import { AppState } from '../AppState';
-import { computed, reactive, onMounted } from 'vue';
+import { computed, reactive, onMounted, ref } from 'vue';
 export default {
-  setup(){
-  return {  }
+  setup() {
+    return {}
   }
 };
 </script>
