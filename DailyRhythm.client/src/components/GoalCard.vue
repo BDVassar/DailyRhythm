07cfdfd3022723359020a0c5dfe7fbde717d2fdd
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-10 16:49:54 -0700

    text color changed

--- DailyRhythm.client/src/components/GoalCard.vue
+++ DailyRhythm.client/src/components/GoalCard.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="{ name: 'GoalDetails', params: { goalId: goal.id } }">
-    <section class="row card opacity-75 text-center p-3" :style="{ backgroundColor: `${goal.color}` }">
+    <section class="row card opacity-75 text-center p-3 text-dark" :style="{ backgroundColor: `${goal.color}` }">
       <h3><span class="mdi" :class="goal.icon"></span>{{ goal.name }}</h3>
       <p>{{ goal.description }}</p>
       <div class="col-12 text-center p-3">
