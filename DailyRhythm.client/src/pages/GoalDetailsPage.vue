94c88be36fc55d3859ea89bdee72d402bc7f809b
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-10 16:51:45 -0700

    buttons added to goal details page

--- DailyRhythm.client/src/pages/GoalDetailsPage.vue
+++ DailyRhythm.client/src/pages/GoalDetailsPage.vue
@@ -1,5 +1,24 @@
 <template>
+    <div class="container-fluid">
+        <div class="row">
+            <router-link class="col-1" :to="{ name: 'Home' }">
+                <h1 class="col-3 d-flex justify-content-start text-white" title="Home"><i
+                        class="mdi mdi-home-circle-outline"></i>
+                </h1>
+            </router-link>
+            <router-link class="col-1" :to="{ name: 'DailyRhythmPage' }">
+                <h1 class="col-3 d-flex justify-content-start text-white" title="DailyRhythm"><i
+                        class="mdi mdi-bullseye-arrow"></i>
+                </h1>
+            </router-link>
+        </div>
+        <div class="row justify-content-evenly">
+            <div v-for="g in goals" class="col-4 p-3">
+                <GoalCard :goal="g" />
+            </div>
+        </div>
 
+    </div>
 </template>
 
 <script>
