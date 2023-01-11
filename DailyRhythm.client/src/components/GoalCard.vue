8831c1bc2f3f0eaf0a3ae29a2a835c337eae9cc3
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-11 14:37:04 -0700

    goals moved to dailyrhythmpage

--- DailyRhythm.client/src/components/GoalCard.vue
+++ DailyRhythm.client/src/components/GoalCard.vue
@@ -3,6 +3,10 @@
     <section class="row card opacity-75 text-center p-3 text-dark" :style="{ backgroundColor: `${goal.color}` }">
       <h3><span class="mdi" :class="goal.icon"></span>{{ goal.name }}</h3>
       <p>{{ goal.description }}</p>
+      <div class="progress">
+        <div class="progress-bar bg-danger" role="progressbar" :style="{ width: `${goal.progress}%` }"
+          :aria-valuenow="goal.progress" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
     </section>
   </router-link>
 </template>
