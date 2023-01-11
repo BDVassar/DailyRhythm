4cd086dd01ecebf990161a818c4c39c7f6c06c0c
Jordan Diaz <jordanldiaz29@gmail.com>
2023-01-11 15:28:46 -0700

    settings page pre-backend version

--- DailyRhythm/server/models/Account.js
+++ DailyRhythm/server/models/Account.js
@@ -8,7 +8,12 @@ export const AccountSchema = new Schema(
     name: { type: String, required: true },
     picture: { type: String },
     // NOTE If you wish to add additional properties do so here
-    bgImage: { type: String, default: 'beach sunset', maxLength: 50 }
+    bgImage: { type: String, required: true, default: 'beach sunset', maxLength: 50 },
+    weatherLocation: { type: String, required: true, default: 'boise' },
+    weatherOn: { type: Boolean, required: true, default: true },
+    clockOn: { type: Boolean, required: true, default: true },
+    timerOn: { type: Boolean, required: true, default: true },
+    quoteType: { type: String, required: true, enum: ['inspiration', 'dadJokes', 'poetry'], default: 'inspiration' }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
