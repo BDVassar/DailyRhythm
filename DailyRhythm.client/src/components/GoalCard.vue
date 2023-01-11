6a97fc6567c4cdc41bdcf914292801061c879339
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-11 10:35:18 -0700

    GoalCard comp updated.

--- DailyRhythm.client/src/components/GoalCard.vue
+++ DailyRhythm.client/src/components/GoalCard.vue
@@ -3,12 +3,6 @@
     <section class="row card opacity-75 text-center p-3 text-dark" :style="{ backgroundColor: `${goal.color}` }">
       <h3><span class="mdi" :class="goal.icon"></span>{{ goal.name }}</h3>
       <p>{{ goal.description }}</p>
-      <div class="col-12 text-center p-3">
-        <h5>Rhythms</h5>
-      </div>
-      <div class="col-12 text-center p-3">
-        <h5>Beats</h5>
-      </div>
     </section>
   </router-link>
 </template>
@@ -32,6 +26,6 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  min-height: 40vh;
+  min-height: 15vh;
 }
 </style>
