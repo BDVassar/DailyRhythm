9adcac2b3372fc566bd9015f297c4a4385cdaabd
Kayla Cammack <kaylacammack@gmail.com>
2023-01-12 16:06:56 -0700

    Updated beat priority

--- DailyRhythm/server/models/Beat.js
+++ DailyRhythm/server/models/Beat.js
@@ -7,7 +7,7 @@ export const BeatSchema = new Schema({
     creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
     description: { type: String, required: false, minLength: 2, maxLength: 1000 },
     completedDate: { type: Date, required: false },
-    priority: { type: String, required: false, enum: ['high', 'medium', 'low',], default: '', lowercase: true },
+    priority: { type: String, required: false, enum: ['high', 'medium', 'low',], default: 'low', lowercase: true },
     accomplished: { type: Boolean, required: true, default: false },
     archived: { type: Boolean, required: true, default: false },
     color: { type: String, default: '#ffaf87' },
